3e9b6c9e1aa3d73bd695ef9a68db5e96cfb9ddf8
Quan <mquannie@gmail.com>
2011-05-04 13:49:20 -0400

    set dictionary path upon gem load

--- lib/grammar_police.rb
+++ lib/grammar_police.rb
@@ -6,4 +6,6 @@ require 'grammar_police/linkage'
 require 'grammar_police/parse_options'
 
 module GrammarPolice
+  #set dictionary path based on the installed gem path
+  GrammarPolice::Dictionary.dictionary_set_data_dir("#{Gem.loaded_specs['grammar_police'].full_gem_path}/data/")
 end
